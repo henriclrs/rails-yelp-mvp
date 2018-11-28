eef2c6fd3789448e9ed13394160043267fd6bcd6
henri clérisse <henri.clerisse@gmail.com>
2018-11-28 14:53:34 +0100

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  namespace :admin do
+    resources :restaurants, only: [:update, :edit, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0404040404',
+    category: 'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0909090909',
+    category: 'japanese',
+  },
+  {
+    name:         'Comptoir volant',
+    address:      '7 Sarasate, Biarritz 64200',
+    phone_number:  '0808080808',
+    category: 'italian',
+  },
+  {
+    name:         "O ch'ti B",
+    address:      '56 rue princesse, Paris 75006',
+    phone_number:  '0707070707',
+    category: 'belgian',
+  },
+  {
+    name:         'Canard street',
+    address:      '7 Boulevard de strasbourg, Lille 59000',
+    phone_number:  '0606060606',
+    category: 'french',
+  },
+  {
+    name:         'Touloulou',
+    address:      '43 rue Saint-André, Lille 59800',
+    phone_number:  '0505050505',
+    category: 'italian',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
